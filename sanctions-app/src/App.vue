474bfd8f5dfd288063d0fba3fd3b08d514fa1db9
EDanilchenko <Danilchenko@outlook.com>
2022-05-28 01:02:15 +0200

    Add Organization module

--- sanctions-app/src/App.vue
+++ sanctions-app/src/App.vue
@@ -2,7 +2,7 @@
 	<div class="menu-bar">
 		<Menubar :model="items">
 			<template #start>
-        <div class="title-container">
+         <div class="title-container">
 				  <img alt="logo" src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png" height="40" class="mr-2">
           <h3>#StopWar</h3>
         </div>
@@ -20,10 +20,8 @@
 import { defineComponent } from 'vue'
 import SearchInput from './components/SearchInput.vue'
 import Menubar from 'primevue/menubar';
-import { usePersonsStore } from './stores/persons';
-import { useTagsStore } from './stores/tags';
-import PersonsService from './service/PersonsService';
-import TagService from './service/TagService';
+import { useMenuStore } from './stores/menu';
+import MenuService from './service/MenuService';
 import ScrollTop from 'primevue/scrolltop';
 
 export default defineComponent({
@@ -32,129 +30,20 @@ export default defineComponent({
     return {
       personsStore: null,
       personsService: null,
-      tagsStore: null,
-      tagService: null,      
-      items: [
-                {
-                   label:'Sanctions',
-                   icon:'pi pi-fw pi-file',
-                   items:[
-                      {
-                         label:'Persons',
-                         icon:'pi pi-fw pi-trash'
-                      },
-                      {
-                         label:'Organizations',
-                         icon:'pi pi-fw pi-external-link'
-                      }
-                   ]
-                },
-                {
-                   label:'Help Ukraine',
-                   icon:'pi pi-fw pi-pencil',
-                   items:[
-                      {
-                         label:'Left',
-                         icon:'pi pi-fw pi-align-left'
-                      },
-                      {
-                         label:'Right',
-                         icon:'pi pi-fw pi-align-right'
-                      },
-                      {
-                         label:'Center',
-                         icon:'pi pi-fw pi-align-center'
-                      },
-                      {
-                         label:'Justify',
-                         icon:'pi pi-fw pi-align-justify'
-                      },
-
-                   ]
-                },
-                {
-                   label:'Help Russia',
-                   icon:'pi pi-fw pi-user',
-                   items:[
-                      {
-                         label:'New',
-                         icon:'pi pi-fw pi-user-plus',
-
-                      },
-                      {
-                         label:'Delete',
-                         icon:'pi pi-fw pi-user-minus',
-
-                      },
-                      {
-                         label:'Search',
-                         icon:'pi pi-fw pi-users',
-                         items:[
-                            {
-                               label:'Filter',
-                               icon:'pi pi-fw pi-filter',
-                               items:[
-                                  {
-                                     label:'Print',
-                                     icon:'pi pi-fw pi-print'
-                                  }
-                               ]
-                            },
-                            {
-                               icon:'pi pi-fw pi-bars',
-                               label:'List'
-                            }
-                         ]
-                      }
-                   ]
-                },
-                {
-                   label:'Media',
-                   icon:'pi pi-fw pi-calendar',
-                   items:[
-                      {
-                         label:'Edit',
-                         icon:'pi pi-fw pi-pencil',
-                         items:[
-                            {
-                               label:'Save',
-                               icon:'pi pi-fw pi-calendar-plus'
-                            },
-                            {
-                               label:'Delete',
-                               icon:'pi pi-fw pi-calendar-minus'
-                            },
-
-                         ]
-                      },
-                      {
-                         label:'Archieve',
-                         icon:'pi pi-fw pi-calendar-times',
-                         items:[
-                            {
-                               label:'Remove',
-                               icon:'pi pi-fw pi-calendar-minus'
-                            }
-                         ]
-                      }
-                   ]
-                },
-                {
-                   label:'About',
-                   icon:'pi pi-fw pi-power-off'
-                }
-             ]      
+      menuStore: null,
+      menuService: null
     }
-  },  
+  }, 
+  computed: {
+     items(){
+        return this.menuStore?.menu;
+     }
+  }, 
   components: {SearchInput, Menubar, ScrollTop},
   mounted() {
-    this.personsStore = usePersonsStore();
-    this.tagsStore = useTagsStore();
-    this.personsService = new PersonsService();
-    this.tagService = new TagService();
-
-    this.personsService.getPersons().then(data => this.personsStore.setPersons(data));
-    this.tagService.getTagNodes().then(data => this.tagsStore.setTags(data));
+   this.menuStore = new useMenuStore();  
+   this.menuService = new MenuService();    
+   this.menuService.getMenu().then(data => this.menuStore.setMenu(data));
   }
 });
 </script>
